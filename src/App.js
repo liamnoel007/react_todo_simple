934d1d79a185fd41ce0350d8c4eccab45a76eea2
liamnoel007 <oasisamb@gmail.com>
2021-11-13 12:17:49 +0300

    Первый пошел

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+//components
+import ToDo from "./components/Todo";
+import ToDoForm from "./components/TodoForms";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTask = (userInput) => {
+    if (userInput) {
+      const newItem = {
+        id: Math.random().toString(36).substr(2, 9),
+        task: userInput,
+        complete: false,
+      };
+      setTodos([...todos, newItem]);
+    }
+  };
+
+  const removeTask = (id) => {
+    setTodos([...todos.filter((todo) => todo.id !== id)]);
+  };
+
+  const handleToggle = (id) => {
+    setTodos([
+      ...todos.map((task) =>
+        task.id === id ? { ...task, complete: !task.complete } : { ...task }
+      ),
+    ]);
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Список задач: {todos.length}</h1>
+      </header>
+      <ToDoForm addTask={addTask} />
+      {todos.map((todo) => {
+        return <ToDo todo={todo} key={todo.id} toggleTask={handleToggle} removeTask={removeTask} />;
+      })}
+    </div>
+  );
+}
+
+export default App;
